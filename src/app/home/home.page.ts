8d69e1ac29ee3e61f920cd114939f87533740e4b
Ken Sodemann <ken@ionic.io>
2019-04-23 12:23:04 -0500

    feat(database): add a tea-categories database
    Just show some basic initialization information with the database

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../services/database/database.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  databaseName: string;
+  databasePath: string;
 
+  constructor(private database: DatabaseService) {}
+
+  async ionViewDidEnter() {
+    await this.database.ready();
+    this.databaseName = this.database.teaCatgories.getName();
+    this.databasePath = await this.database.teaCatgories.getPath();
+  }
 }
