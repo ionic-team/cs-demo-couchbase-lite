697dfc8e4a1a74f4d3c768cf0c66a35c407b4c0b
Ken Sodemann <ken@ionic.io>
2019-04-24 09:06:42 -0500

    feat(tea-categories): remove the test code from the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
-import { DatabaseService } from '../services/database/database.service';
 import { TeaCategory } from '../models/tea-category';
 import { TeaCategoriesService } from '../services/tea-categories/tea-categories.service';
 
@@ -16,16 +15,12 @@ export class HomePage {
   categories: Array<TeaCategory>;
 
   constructor(
-    private database: DatabaseService,
     private teaCategories: TeaCategoriesService,
     private navController: NavController
   ) {}
 
   async ionViewDidEnter() {
     this.categories = await this.teaCategories.getAll();
-    await this.database.ready();
-    this.databaseName = this.database.teaCatgories.getName();
-    this.databasePath = await this.database.teaCatgories.getPath();
   }
 
   addTeaCategory() {
