9a3e08f91f597a1698a79470421c4313c5b4b674
Ken Sodemann <ken@ionic.io>
2019-08-17 17:41:51 -0500

    chore(): upgrade libraries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'tea-category-editor', loadChildren: './tea-category-editor/tea-category-editor.module#TeaCategoryEditorPageModule' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  {
+    path: 'tea-category-editor',
+    loadChildren: () =>
+      import('./tea-category-editor/tea-category-editor.module').then(m => m.TeaCategoryEditorPageModule)
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
