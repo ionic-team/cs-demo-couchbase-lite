753a2594b210d5dfe5bba1d4fe611620cf382856
Ken Sodemann <ken@ionic.io>
2019-08-30 14:49:16 -0500

    chore(): switch to @ionic-enterprise plugin

--- src/app/services/tea-categories/tea-categories.service.ts
+++ src/app/services/tea-categories/tea-categories.service.ts
@@ -11,7 +11,7 @@ import {
   Ordering,
   QueryBuilder,
   SelectResult
-} from 'ionic-enterprise-couchbase-lite';
+} from '@ionic-enterprise/couchbase-lite';
 import { TeaCategory } from '../../models/tea-category';
 
 @Injectable({
